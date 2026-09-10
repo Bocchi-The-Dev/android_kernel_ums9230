fb2064bb7c4c267d9ebfb5e11d227984e1d73d00
Bocchi-The-Dev <mujtabag492@proton.me>
2026-09-10 17:21:44 +0100

    SukiSU sulog: fix minmax.h include on 5.4

--- KernelSU/kernel/sulog/event.c
+++ KernelSU/kernel/sulog/event.c
@@ -2,7 +2,11 @@
 #include <linux/compat.h>
 #include <linux/cred.h>
 #include <linux/gfp.h>
+#if __has_include(<linux/minmax.h>)
 #include <linux/minmax.h>
+#else
+#include <linux/kernel.h>
+#endif
 #include <linux/overflow.h>
 #include <linux/sched/signal.h>
 #include <linux/slab.h>
